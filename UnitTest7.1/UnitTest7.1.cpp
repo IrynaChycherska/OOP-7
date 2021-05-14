b4a5c7692d76ea76da1696867bb2415473861604
unknown <iryna.chycherska.itis.2020@lpnu.ua>
2021-05-14 20:28:04 +0300

    7.1

---
+++ UnitTest7.1/UnitTest7.1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../7.1/Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71
+{
+	TEST_CLASS(UnitTest71)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			array<int, 100> arr = { 1, 2, 5, 7 };
+			Assert::AreEqual(avg(arr, 4), 4.);
+		}
+	};
+}
